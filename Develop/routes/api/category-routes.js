31fdd8e4c96e457c95d8a668428040f1da5681b2
Coco Puff <fahrija_wyroski@live.com>
2021-05-21 18:38:18 -0500

    updated category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,71 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/api/categories', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  db.Category, db.Product.findAll({})
+    .then(dbCategory, dbProduct => res.json(dbCategory, dbProduct))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/api/categories/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  db.Category, db.Product.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategory, dbProduct => {
+    if (!dbCategory, dbProduct) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+    res.json(dbCategory, dbProduct);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post('/api/categories', (req, res) => {
   // create a new category
+  db.Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/api/categories/:id', (req, res) => {
   // update a category by its `id` value
+  db.Category.update({
+    category: req.body.category
+  },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/api/categories/:id', (req, res) => {
   // delete a category by its `id` value
+  db.Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
 module.exports = router;
