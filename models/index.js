9b13fe191d5efefca4a04366bedeffd205db82b5
Coco Puff <fahrija_wyroski@live.com>
2021-05-29 11:05:44 -0500

    update files

--- models/index.js
+++ models/index.js
@@ -14,15 +14,9 @@ Product.belongsTo(Category,
 Category.hasMany(Product, { foreignKey: "category_id" });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
-// Product.hasMany(ProductTag);
-// Product.belongsTo(ProductTag);
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tag_id" });
-
-// Tag.hasMany(ProductTag);
-// // ProductTag belongToMany Tags (through )
-// ProductTag.belongsTo(Tag);
 
 module.exports = {
   Product,
